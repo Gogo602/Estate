fc2c4cad18eff6609bb78403ab6e09310226ddfd
Gogo Benson <151610817+Gogo602@users.noreply.github.com>
2025-04-01 18:53:21 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,15 +5,15 @@ import { motion } from "framer-motion"
 const Header = () => {
   return (
       <div className='h-[100vh] 
-                    flex items-center justify-center w-full overflow-hidden' id='header'>
+                    flex items-center w-full overflow-hidden' id='header'>
       <motion.div
         initial={{ opacity: 0, y: 100 }}
         transition={{ duration: 1.5 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{once:true}}
-        className='py-4 px-6 md:px-20 md:py-28'>
-              <h2 className='text-5xl sm:text-6xl md:text-[82px] 
-              inline-block max-w-3xl font-semibold pt-20 text-blue-500'>Explore Homes that fit your Dreams</h2>
+        className='px-6 md:px-20 mx-auto md:py-28'>
+              <h2 className='text-center text-5xl sm:text-6xl md:text-[82px] 
+              inline-block max-w-3xl font-semibold sm:pt-20 pt-10 text-blue-500'>Explore Homes that fit your Dreams</h2>
               <div className='space-x-6 mt-16'>
                   <a href="#Projects" className='border text-blue-500 border-blue-500 px-8 py-3 
                   rounded'>Projects</a>
