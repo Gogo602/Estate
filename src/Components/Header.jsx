6d7bbbfd7df2bc58d5a823802078c3412620a40d
Gogo Benson <151610817+Gogo602@users.noreply.github.com>
2025-04-01 14:36:09 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -19,9 +19,9 @@ const Header = () => {
               <h2 className='text-5xl sm:text-6xl md:text-[82px] 
               inline-block max-w-3xl font-semibold pt-20'>Explore Homes that fit your Dreams</h2>
               <div className='space-x-6 mt-16'>
-                  <a href="#Projects" className='border  border-white px-8 py-3 
+                  <a href="#Projects" className='border  border-blue-500 px-8 py-3 
                   rounded'>Projects</a>
-                  <a href="#Contact"className='bg-blue-500 px-8 py-3 
+                  <a href="#Contact"className='bg-blue-500 text-blue-50 px-8 py-3 
                   rounded'>Contact Us</a>
               </div>
           </motion.div>
