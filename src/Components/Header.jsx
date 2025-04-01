6d79f6d15862d6ed5fcec7921772fa84624c63d1
Gogo Benson <151610817+Gogo602@users.noreply.github.com>
2025-04-01 14:49:18 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,19 +7,16 @@ const Header = () => {
       <div className='min-h-screen mb-4 bg-cover bg-center
                     flex items-center w-full overflow-hidden' style=
                     {{backgroundImage: "url('src/assets/header_img.png')"}} id='header'>
-
-        <Navbar />
-      
       <motion.div
         initial={{ opacity: 0, y: 100 }}
         transition={{ duration: 1.5 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{once:true}}
-        className='container text-center mx-auto py-4 px-6 md:px-20 py-32'>
+        className='container text-center mx-auto py-4 px-6 md:px-20 py-28'>
               <h2 className='text-5xl sm:text-6xl md:text-[82px] 
               inline-block max-w-3xl font-semibold pt-20 text-blue-500'>Explore Homes that fit your Dreams</h2>
               <div className='space-x-6 mt-16'>
-                  <a href="#Projects" className='border  border-blue-500 px-8 py-3 
+                  <a href="#Projects" className='border text-blue-500 border-blue-500 px-8 py-3 
                   rounded'>Projects</a>
                   <a href="#Contact"className='bg-blue-500 text-blue-50 px-8 py-3 
                   rounded'>Contact Us</a>
