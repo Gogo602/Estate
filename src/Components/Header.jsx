46c18a3774ee27f6c62036edc0871256855e52bd
Gogo Benson <151610817+Gogo602@users.noreply.github.com>
2025-04-01 18:56:31 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
         viewport={{once:true}}
         className='px-6 md:px-20 mx-auto md:py-28'>
               <h2 className='text-center text-5xl sm:text-6xl md:text-[82px] 
-              inline-block max-w-3xl font-semibold sm:pt-20 pt-10 text-blue-500'>Explore Homes that fit your Dreams</h2>
+              inline-block max-w-3xl font-semibold sm:pt-20 pt-5 text-blue-500'>Explore Homes that fit your Dreams</h2>
               <div className='space-x-6 mt-16'>
                   <a href="#Projects" className='border text-blue-500 border-blue-500 px-8 py-3 
                   rounded'>Projects</a>
