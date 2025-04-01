42d6a377864cbdb7c314314e47f493a3d2845059
Gogo Benson <151610817+Gogo602@users.noreply.github.com>
2025-04-01 14:30:48 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,15 +5,17 @@ import { motion } from "framer-motion"
 const Header = () => {
   return (
       <div className='min-h-screen mb-4 
-                    flex items-center w-full overflow-hidden' id='header'>
-     
+                    flex items-center w-full overflow-hidden' style=
+                    {{backgroundImage: "url('src/assets/header_img.png')"}} id='header'>
+
+        <Navbar />
       
       <motion.div
         initial={{ opacity: 0, y: 100 }}
         transition={{ duration: 1.5 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{once:true}}
-        className='container text-center mx-auto py-4 px-6 md:px-20 py-32 text-white'>
+        className='container text-center mx-auto py-4 px-6 md:px-20 py-32 text-blue-500'>
               <h2 className='text-5xl sm:text-6xl md:text-[82px] 
               inline-block max-w-3xl font-semibold pt-20'>Explore Homes that fit your Dreams</h2>
               <div className='space-x-6 mt-16'>
