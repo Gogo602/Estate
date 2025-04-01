e10a6a69dc4bb78eb7cc4787ed4387d5c4610c71
Gogo Benson <151610817+Gogo602@users.noreply.github.com>
2025-04-01 14:32:21 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion"
 
 const Header = () => {
   return (
-      <div className='min-h-screen mb-4 
+      <div className='min-h-screen mb-4 bg-cover bg-center
                     flex items-center w-full overflow-hidden' style=
                     {{backgroundImage: "url('src/assets/header_img.png')"}} id='header'>
 
